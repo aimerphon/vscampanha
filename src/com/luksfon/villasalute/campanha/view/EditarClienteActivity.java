9d078344d631406ff0a9e756b5d0cfdd8ae27248
LucasFon <LucasFon@XPS15WIN8>
2015-06-27 11:43:36 -0300

    Criação da edição de cliente.

---
+++ src/com/luksfon/villasalute/campanha/view/EditarClienteActivity.java
@@ -0,0 +1,25 @@
+package com.luksfon.villasalute.campanha.view;
+
+import com.luksfon.villasalute.campanha.R;
+
+import android.os.Bundle;
+
+public class EditarClienteActivity extends BaseActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		layoutResId = R.layout.cadastrar_cliente;
+		
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	protected void inicializarTela() {
+		
+	}
+
+	@Override
+	protected void carregarTela() {
+		
+	}
+}
